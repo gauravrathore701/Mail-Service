7491f4840b2f81d1225a814677e996dd70ad1462
gaurav <killingwiz@gmail.com>
2026-01-31 14:48:47 +0000

    port change

--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,6 @@ async fn main() {
         .layer(cors)  // Add CORS layer HERE
         .with_state(state);
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:7070").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
